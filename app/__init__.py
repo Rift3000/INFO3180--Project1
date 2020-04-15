b4c8492460a94e24a6e656aaa956f7a9bf34c729
Jason Williams <williamsjacen007@gmail.com>
2020-04-15 12:13:04 -0500

    go23

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "whatPassword?"
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://djcviaztdjzsxe:102ffe4635fe1f75902016e471af8a06806c75fa5fad98014d9db7114950a533@ec2-34-197-212-240.compute-1.amazonaws.com:5432/dsgnh8rsq3vnu"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://ppyljwauodikmn:bac19222a3bdc5209dc823ce2a22f5fe7800b73640aa85aaebf6003d85fa2ebb@ec2-3-211-48-92.compute-1.amazonaws.com:5432/dk0nohq8t1kq3"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True # added just to suppress a warning
 
 UPLOAD_FOLDER = './app/static/uploads'
